789bcb1d000e60ddb10f6e61cd1089b6c670ebe8
glen ordóñez <149749777+glen-ob@users.noreply.github.com>
2026-05-10 17:55:26 -0600

    prueba backend

--- backend/services/server.js
+++ backend/services/server.js
@@ -188,7 +188,9 @@ app.get('/check-image/:filename', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`\n========================================`);
+// app.listen(PORT, () => {
+//     console.log(`\n========================================`);
     
-});
+// });
+
+export default app;
